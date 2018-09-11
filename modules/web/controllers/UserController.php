44dd905c7f0bca20ef4d927c1a1e2366f4e38d51
AilonHuang <529678737@qq.com>
2018-09-11 22:28:23 +0800

    用户登陆 编辑信息 重置密码 模板

---
+++ modules/web/controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\modules\web\controllers;
+
+use yii\web\Controller;
+
+class UserController extends Controller
+{
+    // 登陆页面
+    public function actionLogin()
+    {
+        $this->layout = false;
+        return $this->render('login');
+    }
+
+    // 编辑当前登录人信息
+    public function actionEdit()
+    {
+        $this->layout = false;
+        return $this->render('edit');
+    }
+
+    // 重置当前登录人密码
+    public function actionResetPwd()
+    {
+        $this->layout = false;
+        return $this->render('reset_pwd');
+    }
+}
