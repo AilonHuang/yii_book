cf4e27d08cc625bd911fbd0a1d6d9e202dc9e134
AilonHuang <529678737@qq.com>
2018-09-12 23:39:37 +0800

    我的购物车 模板

--- modules/m/controllers/UserController.php
+++ modules/m/controllers/UserController.php
@@ -6,9 +6,17 @@ use yii\web\Controller;
 
 class UserController extends Controller
 {
+    // 账号绑定
     public function actionBind()
     {
         $this->layout = false;
         return $this->render('bind');
+    }
+
+    // 我的购物车
+    public function actionCart()
+    {
+        $this->layout = false;
+        return $this->render('cart');
     }
 }
